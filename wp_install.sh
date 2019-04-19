9659226bfd492b432d7a0b26305406151ed1376e
atrandys <37266675+atrandys@users.noreply.github.com>
2019-04-19 21:14:20 +0800

    Update wp_install.sh

--- wp_install.sh
+++ wp_install.sh
@@ -290,6 +290,7 @@ uninstall_wp(){
     read -s -n1 -p "按回车键开始卸载，按ctrl+c取消"
     yum remove -y php70w php70w-mysql php70w-gd php70w-xml php70w-fpm mysql nginx
     rm -rf /usr/share/nginx/html/*
+    rm -rf /var/lib/mysql
     green "=========="
     green " 卸载完成"
     green "=========="
